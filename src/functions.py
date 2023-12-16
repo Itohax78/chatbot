71f989d05e08c07801af4abae0b232fb4030810f
Itohax78 <150530730+Itohax78@users.noreply.github.com>
2023-12-16 22:36:27 +0100

    dd

--- src/functions.py
+++ src/functions.py
@@ -79,10 +79,10 @@ def cleaned(directory):
         for filename in os.listdir(name_file):
             if filename.endswith("txt"):
                 file_path = os.path.join(name_file, filename)
-                with open(file_path, 'r') as file:
+                with open(file_path, 'r', encoding='utf-8') as file:
                     content = file.read()
                 content_lower = content.lower()
-                with open(file_path, 'w') as cleaned_file:
+                with open(file_path, 'w', encoding='utf-8') as cleaned_file:
                     cleaned_file.write(content_lower)
 
 #Permet de créer une liste qui stocke tous les noms des fichiers présents dans le répertoire "cleaned"
@@ -100,11 +100,11 @@ def remove_punctuation_character():
         files_names = store_files(name_file, ".txt")
         for files in files_names:
             file_path = os.path.join(name_file, files)
-            with open(file_path, "r") as text:
+            with open(file_path, "r", encoding='utf-8') as text:
                 content = text.read()
                 for char in string.punctuation:
                     content = content.replace(char, ' ')
-            with open(file_path, 'w') as cleaned_text:
+            with open(file_path, 'w', encoding='utf-8') as cleaned_text:
                 cleaned_text.write(content)
 
 #Permet de calculer le score TF d'un mot/phrase choisis
@@ -138,7 +138,7 @@ def idf(directory):
             words_in_file = set()
 
             #Permet de compter les mots uniques dans chaque fichier
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r', encoding='utf-8') as file:
                 content = file.read().lower().split()
                 words_in_file.update(content)
 
@@ -149,10 +149,11 @@ def idf(directory):
     #Permet de calculer le score IDF pour chaque mot
     idf_scores = {}
     for word, doc_count in word_documents_count.items():
-        idf = math.log(total_documents / doc_count)
+        idf = math.log10(total_documents / doc_count)
         idf_scores[word] = idf
 
     return idf_scores
+
 
 #Permet de calculer le score TF-IDF
 def calculate_tf_idf_scores(content, directory):
@@ -165,153 +166,125 @@ def calculate_tf_idf_scores(content, directory):
     return tf_idf_scores
 
 #Permet de créer la matrice TF-IDF avec en colonne les documents et en ligne les mots avec leur score TF-IDF associé
-'''def calculate_tf_idf_matrix(directory):
-    word_index = {}  #Dictionnaire pour stocker les index des mots uniques
-    index = 0
-    tf_idf_matrix = []  #Matrice pour stocker les valeurs TF-IDF
-
+def build_tf_idf_matrix(directory):
     idf_scores = idf(directory)
+    vocabulary = list(idf_scores.keys())
+    tf_idf_matrix = {}
 
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         if os.path.isfile(file_path) and filename.endswith('.txt'):
-            with open(file_path, 'r') as file:
-                content = file.read()
-                tf_scores = tf(content)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read().lower()
 
-                for word, tf_value in tf_scores.items():
-                    if word not in word_index:
-                        word_index[word] = index
-                        index += 1
+            # Calculer les scores TF pour le document courant
+            tf_scores = tf(content)
 
-    #Permet d'initialiser la matrice TF-IDF avec des zéros
-    for j in range(len(word_index)):
-        tf_idf_matrix.append([0] * len(os.listdir(directory)))
+            # Construire le vecteur TF-IDF pour le document courant
+            tf_idf_vector = {word: tf_scores.get(word, 0) * idf_scores.get(word, 0) for word in vocabulary}
+            tf_idf_matrix[filename] = tf_idf_vector
 
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path) and filename.endswith('.txt'):
-            with open(file_path, 'r') as file:
-                content = file.read()
-                tf_scores = tf(content)
+    return vocabulary, tf_idf_matrix
 
-                for word, tf_value in tf_scores.items():
-                    word_idx = word_index[word]
-                    tf_idf_matrix[word_idx][index] = tf_value * idf_scores.get(word, 0)
 
-    #Permet de calculer la transposée de la matrice (échange des lignes et colonnes)
-    tf_idf_transpose = []
-    for col in range(len(tf_idf_matrix[0])):
-        tf_idf_transpose.append([tf_idf_matrix[row][col] for row in range(len(tf_idf_matrix))])
+def find_least_important_words(tf_idf_matrix):
+    least_important_words = set()  # Utiliser un ensemble pour éviter les doublons
 
-    return tf_idf_transpose'''
+    # Initialiser l'ensemble avec tous les mots du premier document
+    first_doc_scores = next(iter(tf_idf_matrix.values()))
+    for word in first_doc_scores:
+        least_important_words.add(word)
 
-'''def recover_string_file(directory, file):
-    file_path = os.path.join(directory, file)
-    with open(file_path, 'r') as file:
-        content = file.read()
-def matrice_tf_idf(directory):
-    """
-     calcule la matrice TF-IDF pour tous les fichiers d'un répertoire.
-    """
-    # Dictionnaire pour la matrice TF-IDF.
-    tf_idf_matrice = {}
-    # Calcul des scores IDF.
-    idf_scores = idf(directory)
-    # Liste des fichiers.
-    files = os.listdir(directory)
-    for filename in files:
+    # Parcourir la matrice TF-IDF pour chaque document
+    for doc_scores in tf_idf_matrix.values():
+        for word in least_important_words.copy():  # Créer une copie pour modifier l'ensemble pendant l'itération
+            # Si un mot a un score TF-IDF > 0 dans ce document, le retirer de l'ensemble
+            if doc_scores.get(word, 0) > 0:
+                least_important_words.discard(word)
+
+    return list(least_important_words)  # Convertir l'ensemble en liste avant de retourner
+
+def find_highest_tf_idf_words(tf_idf_matrix):
+    highest_score = 0
+    highest_words = []
+
+    # Parcourir tous les documents
+    for doc_scores in tf_idf_matrix.values():
+        # Parcourir tous les mots et leurs scores TF-IDF
+        for word, score in doc_scores.items():
+            # Si le score actuel est plus élevé que le score le plus élevé enregistré
+            if score > highest_score:
+                highest_score = score
+                highest_words = [word]
+            # Si le score actuel est égal au score le plus élevé, ajouter le mot à la liste
+            elif score == highest_score:
+                highest_words.append(word)
+
+    return highest_words, highest_score
+
+def find_most_repeated_words_by_chirac(tf_idf_matrix, least_important_words):
+    word_frequency = {}
+
+    for filename, doc_scores in tf_idf_matrix.items():
+        # Vérifiez si le fichier est l'un des discours de Chirac
+        if 'Nomination_Chirac1.txt' in filename or 'Nomination_Chirac2.txt' in filename:
+            for word, score in doc_scores.items():
+                if word not in least_important_words:
+                    if word not in word_frequency:
+                        word_frequency[word] = 0
+                    word_frequency[word] += 1  # Incrementer la fréquence du mot
+
+    # Trouver le mot le plus répété
+    most_repeated_word = max(word_frequency, key=word_frequency.get, default=None)
+    most_repeated_count = word_frequency.get(most_repeated_word, 0)
+
+    return most_repeated_word, most_repeated_count
+
+
+
+def find_presidents_mentions_of_nation(tf_idf_matrix):
+    # Initialiser un dictionnaire pour compter les mentions de "nation" pour chaque président
+    president_nation_mentions = {}
+
+    # Parcourir tous les documents pour compter les mentions pour chaque président
+    for filename, doc_scores in tf_idf_matrix.items():
+        # Extraire le nom du président du nom du fichier
+        president_name = filename.replace('Nomination_', '').replace('.txt', '')
+
+        # Compter les mentions de "nation"
+        president_nation_mentions[president_name] = president_nation_mentions.get(president_name, 0) + doc_scores.get(
+            'nation', 0)
+
+    # Trouver le président qui a mentionné "nation" le plus de fois
+    most_mentions = max(president_nation_mentions, key=president_nation_mentions.get)
+    most_mentions_count = president_nation_mentions[most_mentions]
+
+    return president_nation_mentions, most_mentions, most_mentions_count
+
+def find_first_mention_of_ecology_or_climate(cleaned_directory):
+    # Liste des mots à rechercher
+    keywords = ['climat', 'écologie', 'environnement']
+
+    # Parcourir les fichiers dans l'ordre alphabétique, qui peut correspondre à l'ordre chronologique
+    for filename in sorted(os.listdir(cleaned_directory)):
         if filename.endswith('.txt'):
-            # Calcul des scores TF pour le fichier actuel.
-            tf_scores = tf(recover_string_file(directory, filename))
-            for mot, tf in tf_scores.items():
-                # Calcul du score TF-IDF.
-                tf_idf = tf * idf_scores[mot]
-                if mot in tf_idf_matrice:
-                    tf_idf_matrice[mot].append(tf_idf)
-                else:
-                    tf_idf_matrice[mot] = [tf_idf]
+            with open(os.path.join(cleaned_directory, filename), 'r', encoding='utf-8') as file:
+                content = file.read().lower()
+                # Vérifier si l'un des mots-clés est mentionné dans le discours
+                if any(keyword in content for keyword in keywords):
+                    # Extraire le nom du président du nom du fichier
+                    president_name = filename.replace('Nomination_', '').replace('.txt', '').split('_')[0]
+                    return president_name, filename
 
-    return tf_idf_matrice'''
+    return None, None
 
-#fonctionnalité de base à développer
+def find_common_words_among_all_presidents(tf_idf_matrix, least_important_words):
+    # Initialiser un ensemble avec tous les mots du premier document
+    common_words = set(tf_idf_matrix[next(iter(tf_idf_matrix))].keys()) - set(least_important_words)
 
-def moinsimportant(directory, num_lowest_scores=10):
-    all_tf_idf_scores = {}
-    words_with_zero_tf_idf = {}
+    # Intersecter les ensembles de mots de tous les documents pour trouver les mots communs
+    for doc_scores in tf_idf_matrix.values():
+        current_doc_words = set(doc_scores.keys()) - set(least_important_words)
+        common_words = common_words.intersection(current_doc_words)
 
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path) and filename.endswith('.txt'):
-            with open(file_path, 'r') as file:
-                content = file.read()
-                tf_idf_scores = calculate_tf_idf_scores(content, directory)
-
-                # Mettre à jour tous les scores TF-IDF calculés
-                all_tf_idf_scores.update(tf_idf_scores)
-
-    # Filtrer les mots avec un score TF-IDF égal à zéro
-    words_with_zero_tf_idf = {word: score for word, score in all_tf_idf_scores.items() if score == 0}
-
-    return words_with_zero_tf_idf
-def highest_score(directory):
-    all_tf_idf_scores = {}
-    max_tf_idf_score = float('-inf')
-    words_with_max_tf_idf = {}
-
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path) and filename.endswith('.txt'):
-            with open(file_path, 'r') as file:
-                content = file.read()
-                tf_idf_scores = calculate_tf_idf_scores(content, directory)
-
-                # Mettre à jour tous les scores TF-IDF calculés
-                all_tf_idf_scores.update(tf_idf_scores)
-
-    # Trouver le score TF-IDF le plus élevé parmi tous les mots
-    max_tf_idf_score = max(all_tf_idf_scores.values())
-
-    # Filtrer les mots avec un score TF-IDF égal au score maximal
-    words_with_max_tf_idf = {word: score for word, score in all_tf_idf_scores.items() if score == max_tf_idf_score}
-
-    return words_with_max_tf_idf
-
-
-def chirac_repetition():
-    with open('discours_chirac_1.txt', 'r') as file1, open('discours_chirac_2.txt', 'r') as file2:
-        text1 = file1.read()
-        text2 = file2.read()
-
-    combined_text = text1 + ' ' + text2
-    compteur_mots_combined = (combined_text.split())
-    mots_plus_frequents_combined = compteur_mots_combined.most_common(5)
-    print("Les mots les plus fréquents dans les discours de Chirac sont :", mots_plus_frequents_combined)
-
-
-
-'''discours_presidents = [
-    "Nomination_Chirac2.txt",
-    "Nomination_Giscard dEstaing.txt",
-    "Nomination_Hollande.txt",
-    "Nomination_Macron.txt",
-    "Nomination_Mitterand1.txt",
-    "Nomination_Mitterand2.txt",
-    "Nomination_Sarkozy.txt",
-
-]
-
-mots_par_president = []
-
-for discours in discours_presidents:
-    with open(discours, 'r') as file:
-        text = file.read()
-        text = text.lower()
-        text = text.translate(str.maketrans('', '', string.punctuation))
-        mots = text.split()
-        mots = [mot for mot in mots if mot not in ["le", "la", "les", "de", "du", "des", "et", "que", "qui", "à", "un", "une", "il", "elle", "nous", "vous"]]
-        mots_par_president.append(set(mots))
-mots_communs = set.intersection(*mots_par_president)
-
-print("Mots communs à tous les présidents :", mots_communs)
-'''
+    return common_words
--- src/main.py
+++ src/main.py
@@ -2,42 +2,58 @@
 import functions
 import time
 
-matrix = functions.m
-print(matrix)
-
-
 # Le menu utilisateur
 def afficher_menu():
-    print("############")
-    print("# ChatBot  #")
-    print("############")
+    print("|----------------|")
+    print("|    ChatBot     |")
+    print("|----------------|")
     time.sleep(1)
     # Les différentes options pour les utilisateurs
     print("1. Afficher la liste des mots les moins importants")
     print("2. Afficher les mots ayant le score TF-IDF le plus élevé")
     print("3. Afficher les mots les plus répétés par le président Chirac")
-    print(
-        """4. Afficher les noms des présidents qui ont parlé de la "Nation" et de celui qui l'a répété le plus de fois""")
+    print("""4. Afficher les noms des présidents qui ont parlé de la "Nation" et de celui qui l'a répété le plus de fois""")
     print("5. Affiche le premier président à parler du climat et de l'écologie")
     print("6. Affiche les mots que tous les présidents ont évoqués")
     print("7. Quitter")
 
 
 def option1():
-    print("voici les mots avec les plus bas score")
-    opt1 = moinsimportant("C:\AppData\chatbot\speeches\cleaned")
-    print(opt1)
-
+    vocabulary, tf_idf_matrix = functions.build_tf_idf_matrix('C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned')
+    least_important_words = functions.find_least_important_words(tf_idf_matrix)
+    print("Les mots les moins importants sont :", least_important_words)
 
 def option2():
-    print("voici le mot avec le plus haut score : ")
-    opt2 = highest_score("C:\AppData\chatbot\speeches\cleaned")
-    print(opt2)
-
+    vocabulary, tf_idf_matrix = functions.build_tf_idf_matrix('C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned')
+    highest_words, highest_score = functions.find_highest_tf_idf_words(tf_idf_matrix)
+    print("Le mot avec le score TF-IDF le plus élevé est :", highest_words, "avec un score de :", highest_score)
 
 def option3():
-    opt3 = chirac_repetition("CC:\AppData\chatbot\speeches\cleaned")
-    print(opt3)
+    vocabulary, tf_idf_matrix = functions.build_tf_idf_matrix('C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned')
+    least_important_words = functions.find_least_important_words(tf_idf_matrix)
+    most_repeated_word, most_repeated_count = functions.find_most_repeated_words_by_chirac(tf_idf_matrix, least_important_words)
+    print("Le mot le plus répété par le président Chirac est :", most_repeated_word, "avec une fréquence de :", most_repeated_count)
+
+def option4():
+
+    vocabulary, tf_idf_matrix = functions.build_tf_idf_matrix('C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned')
+    president_nation_count, president_with_most_mentions, most_mentions_count = functions.find_presidents_mentions_of_nation(tf_idf_matrix)
+    print("Nombre de mentions de 'nation' par président :", president_nation_count)
+    print(f"Le président ayant le plus mentionné 'nation' est : {president_with_most_mentions} avec {most_mentions_count} mentions.")
+
+def option5():
+    cleaned_directory = 'C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned'
+    first_president, speech_file = functions.find_first_mention_of_ecology_or_climate(cleaned_directory)
+    if first_president:
+        print(f"Le premier président à parler du climat ou de l'écologie est {first_president} dans le fichier {speech_file}.")
+    else:
+        print("Aucune mention du climat ou de l'écologie trouvée dans les discours.")
+
+def option6():
+    vocabulary, tf_idf_matrix = functions.build_tf_idf_matrix('C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned')
+    least_important_words = functions.find_least_important_words(tf_idf_matrix)
+    common_words = functions.find_common_words_among_all_presidents(tf_idf_matrix, least_important_words)
+    print(f"Les mots mentionnés par tous les présidents sont : {common_words}")
 
 
 # Permet d'afficher une animation de chargement
@@ -64,11 +80,11 @@ def menu():
             #  afficher_animation()
             option3()
         elif choix == '4':
-            option3()
+            option4()
         elif choix == '5':
-            option3()
+            option5()
         elif choix == '6':
-            option3()
+            option6()
         elif choix == '7':
             print("\nAu revoir !")
             break  # Permet d'arreter le système
@@ -80,38 +96,37 @@ def menu():
 
 menu()
 
-extraire_nom = extraction()
+'''extraire_nom = functions.extraction()
 print(extraire_nom)
 
-name = president(list_of_files("speeches", ".txt"))
+name = functions.president(functions.list_of_files("speeches", ".txt"))
 print(name)
 
-number = cancel_number(name)
+number = functions.cancel_number(name)
 print(number)
 
-noduplicate = duplicate_filter(number)
+noduplicate = functions.duplicate_filter(number)
 print(noduplicate)
 
-prenom = first_name(noduplicate)
+prenom = functions.first_name(noduplicate)
 print(prenom)
 
-directory_exist("C:\AppData\chatbot\speeches")
+functions.directory_exist("C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\speeches")
 
-cleaned = cleaned("C:\AppData\chatbot\speeches")
+cleaned = functions.cleaned("C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\speeches")
 print(cleaned)
 
-store_files("cleaned", ".txt")
+functions.store_files("cleaned", ".txt")
 
-remove_punctuation_character()
-idf = idf("C:\AppData\chatbot\speeches\cleaned")
+functions.remove_punctuation_character()
+
+idf = functions.idf("C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned")
 print(idf)
 
 Words = input("Saisisez le mot à calculer :")
-tfidf = calculate_tf_idf_scores(Words, "C:/users/User\Desktop\chatbot\cleaned")
+tfidf = functions.calculate_tf_idf_scores(Words, "C:/Users/antpe\OneDrive\Documents\GitHub\chatbot\src\cleaned")
 print("le score est de :", tfidf)
 
-matrix = matrice_tf_idf("C:\AppData\chatbot\speeches\cleaned")
-print(matrix)
-
-fnct1 = moinsimportant("C:\AppData\chatbot\speeches\cleaned")
-print(fnct1)
+vocabulary, tf_idf_matrix = functions.build_tf_idf_matrix('cleaned')
+print(tf_idf_matrix)
+'''
